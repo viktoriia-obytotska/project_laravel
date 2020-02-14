6ce884c069f4ae545c8bc669709c58f1b56c19fa
viktoriia-obytotska <viktoriaobytotska@gmail.com>
2020-02-14 19:07:19 +0200

    install package botman

---
+++ studio/config/botman/telegram.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Telegram Token
+    |--------------------------------------------------------------------------
+    |
+    | Your Telegram bot token you received after creating
+    | the chatbot through Telegram.
+    |
+    */
+    'token' => env('TELEGRAM_TOKEN'),
+);
---
+++ studio/database/migrations/2020_02_14_125640_create_establishments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEstablishmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('establishments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 35);
+            $table->integer('category_id')->unsigned();
+            $table->string('image');
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('establishments');
+    }
+}
